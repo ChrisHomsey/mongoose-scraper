8032a1918733f51d574ec67fc815c6d228ad1121
Chris <chomsey1@gmail.com>
2018-03-11 20:31:47 -0400

    format changing

--- public/assets/js/saved.js
+++ public/assets/js/saved.js
@@ -1,9 +1,9 @@
 // JSON get request to grab all saved headlines and append them to the #feed - also grabs the number of saved articles to display them on the navbar
 $.getJSON("/api/saved", function(data) {
     // For each one
+    $("#saved-num").text(data.length);
     for (var i = 0; i < data.length; i++) {
         console.log(data[i]);
-        $("#saved-num").text(data.length);
         $("#feed").append('<div class="column"><div class="ui fluid card"><div class="image"><a class="header" href=' + data[i].link + '><img class="fluid" src="' + data[i].thumbnail + '"></a></div><div class="content"><a class="header" href="' + data[i].link + '">' + data[i].title + '</a></div><div class="ui small bottom attached red button unsave-btn" data-id="'  + data[i]._id + '"><i class="minus icon"></i>Remove from Saved</div></div></div>');
     }
 });
